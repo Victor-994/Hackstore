d79667038383011d39ba57254109f69a6576b35f
Victor Adejumo <78537021+Victor-994@users.noreply.github.com>
2025-04-23 10:48:03 +0100

    api-change

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000";
+const API_URL = "https://hack-backend-lcsb.onrender.com";
 
 export const fetchProducts = async () => {
   const res = await fetch(`${API_URL}/products`);
